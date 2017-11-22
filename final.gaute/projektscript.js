9ead31f51472ba05aca35a517222e6ffa54debe2
Gaute Tessand Baalsrud <gaute.tb@gmail.com>
2017-11-22 12:36:53 +0100

    commiting changes

---
+++ final.gaute/projektscript.js
@@ -0,0 +1,22 @@
+
+var x = 0;   
+function increment(value) {
+    document.getElementById('blue').innerHTML = ++x;
+}
+
+function decrement(value) {
+    document.getElementById('blue').innerHTML = --x;
+}
+
+var y = 0;
+
+function increment(value) {
+document.getElementById('green').innerHTML = ++y;
+}
+
+function decrement(value) {
+document.getElementById('green').innerHTML = --y;
+}
+
+
+
